ef264f01305832e97549f3583eadec152d9fc461
Cedric Patton <ceddyp117@gmail.com>
2020-06-30 06:40:37 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -50,7 +50,12 @@ def add_characters
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
+      },
+      :additional_characters => [
+        {:name => "Prince Escalus"},
+        {:name => "Apothecary"}
+      
+        ]
    }
 
    # Write code below that does the following programmatically:
